326f848e7e8a22dcac3dafa8c7e5d99970765c0a
MuradArikainen <arikainen@list.ru>
2023-05-26 12:11:47 +0300

    Сделали звонилку через ARI

---
+++ ast_web_api.py
@@ -0,0 +1,3 @@
+import requests
+
+result = requests.post(http://10.10.103.233:8088/ari/channels?endpoint=sip%2F101&extension=102&context=my&priority=1&label=101&callerId=101&timeout=30&api_key=ariuser:123456)
